bb7bf5d0e1dbf71544654b6792cada95a44e19af
Adrian Croitoru <adrian.croitoru.97@gmail.com>
2024-11-09 19:33:45 +0200

    init - backend

---
+++ backend/src/main/java/com/ugabuga/android_poi/AndroidPoiApplication.java
@@ -0,0 +1,13 @@
+package com.ugabuga.android_poi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidPoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidPoiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ugabuga/android_poi/AndroidPoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugabuga.android_poi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidPoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
